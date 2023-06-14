e0106921593886afa24ea047739b7f1dc8b55935
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-06-14 21:22:43 +0530

    Edit

---
+++ components/CustomButton.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+const CustomButton = ({title,containerStyles,handleClick}) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={"flex-1"}>{title}</span>
+    </button>
+  );
+};
+export default CustomButton;
